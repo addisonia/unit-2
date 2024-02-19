54ee5baab4ec3edac539435a261d1e70dea52a5e
addison atkin <addisonathome@gmail.com>
2024-02-19 14:20:19 -0600

    updated adaptedTutorial.js

--- boilerplate/js/adaptedTutorial.js
+++ boilerplate/js/adaptedTutorial.js
@@ -1,149 +1,37 @@
-// Define a GeoJSON feature object with its properties and geometry.
-var geojsonFeature = {
-    "type": "Feature",
-    "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
-    }
-};
+// Function to instantiate the Leaflet map
+function createMap(){
+    // Create the map and set its view to a given geographical coordinates and zoom level
+    var map = L.map('map', {
+        center: [20, 0],
+        zoom: 2
+    });
 
-// Define an array of line geometries in GeoJSON format.
-var myLines = [{
-    "type": "LineString",
-    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
-}, {
-    "type": "LineString",
-    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
-}];
+    // Add OpenStreetMap tile layer to the map
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(map);
 
-// Define a style object to be used for lines.
-var myStyle = {
-    "color": "#ff7800",
-    "weight": 5,
-    "opacity": 0.65
-};
-
-// Define an array of GeoJSON feature objects with polygon geometries.
-var states = [{
-    "type": "Feature",
-    "properties": {"party": "Republican"},
-    "geometry": {
-        "type": "Polygon",
-        "coordinates": [[
-            [-104.05, 48.99],
-            [-97.22,  48.98],
-            [-96.58,  45.94],
-            [-104.03, 45.94],
-            [-104.05, 48.99]
-        ]]
-    }
-}, {
-    "type": "Feature",
-    "properties": {"party": "Democrat"},
-    "geometry": {
-        "type": "Polygon",
-        "coordinates": [[
-            [-109.05, 41.00],
-            [-102.06, 40.99],
-            [-102.03, 36.99],
-            [-109.04, 36.99],
-            [-109.05, 41.00]
-        ]]
-    }
-}];
-
-// Define marker options for GeoJSON point features.
-var geojsonMarkerOptions = {
-    radius: 8,
-    fillColor: "#ff7800",
-    color: "#000",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8
-};
-
-// Define a function to run on each feature before adding it to the GeoJSON layer, to bind a popup if necessary.
-function onEachFeature(feature, layer) {
-    if (feature.properties && feature.properties.popupContent) {
-        layer.bindPopup(feature.properties.popupContent);
-    }
+    // Call the getData function to load and display the GeoJSON data
+    getData(map);
 }
 
-// Define a filter function to control the visibility of features based on their properties.
-function filterFeature(feature, layer) {
-    return feature.properties.show_on_map;
+// Function to retrieve the GeoJSON data and add it to the map
+function getData(map){
+    // Load the data with fetch, then add a GeoJSON layer to the map
+    fetch("data/MegaCities.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            // Create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(json, {
+                onEachFeature: function(feature, layer) {
+                    // Bind popups to each feature, for example, displaying the city's name
+                    layer.bindPopup(feature.properties.City);
+                }
+            }).addTo(map);
+        });
 }
 
-// Define an array of GeoJSON feature objects for point geometries.
-var someFeatures = [{
-    "type": "Feature",
-    "properties": {
-        "name": "Coors Field",
-        "show_on_map": true,
-        "popupContent": "This is where the Rockies play!"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
-    }
-}, {
-    "type": "Feature",
-    "properties": {
-        "name": "Busch Field",
-        "show_on_map": false
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.98404, 39.74621]
-    }
-}];
-
-// Initialize the map.
-var map = L.map('map').setView([39.76, -104.99], 13);
-
-// Add tile layer to the map
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-// Add the GeoJSON feature to the map
-L.geoJSON(geojsonFeature, {
-    onEachFeature: onEachFeature
-}).addTo(map);
-
-// Add lines to the map with the specified style
-L.geoJSON(myLines, {
-    style: myStyle
-}).addTo(map);
-
-// Add polygons to the map with conditional styles based on their properties
-L.geoJSON(states, {
-    style: function(feature) {
-        switch (feature.properties.party) {
-            case 'Republican': return {color: "#ff0000"};
-            case 'Democrat': return {color: "#0000ff"};
-        }
-    }
-}).addTo(map);
-
-// Add point features to the map with a custom marker
-L.geoJSON(someFeatures, {
-    filter: filterFeature,
-    pointToLayer: function(feature, latlng) {
-        return L.circleMarker(latlng, geojsonMarkerOptions);
-    },
-    onEachFeature: onEachFeature
-}).addTo(map);
-
-// Listen for map clicks and pop up a message
-map.on('click', function(e) {
-    L.popup()
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
-});
+// Event listener that runs the createMap function once the DOM is loaded
+document.addEventListener('DOMContentLoaded', createMap);
