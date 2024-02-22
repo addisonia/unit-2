e5c9772077273e958bec592bc1d770583476eaaf
addison atkin <addisonathome@gmail.com>
2024-02-22 17:35:19 -0600

    added sliders that work and fixed up geojson data to only include 7 population points instead of 18. added style.css file and updated index.html file.

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,91 +1,127 @@
+
 // Function to instantiate the Leaflet map
 function createMap(){
-    // Create the map and set its view to a given geographical coordinates and zoom level
     var map = L.map('map', {
         center: [37.8, -96], // Adjusted center to USA
         zoom: 4 // Adjusted zoom level to show the USA
     });
 
-    // Add OpenStreetMap tile layer to the map
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
     }).addTo(map);
 
-    // Call the getData function to load and display the GeoJSON data
     getData(map);
 }
 
-// Function to calculate the radius of the circle markers
 function calcPropRadius(attValue) {
-    // Adjust the scaleFactor to increase the size of the markers
-    var scaleFactor = 0.0005; // Adjust this value to change the symbol sizes
+    var scaleFactor = 0.0005;
     var area = attValue * scaleFactor;
-    // Apply a square root to reduce the disparity in sizes
-    return Math.sqrt(area/Math.PI) * 2; // The multiplier can be adjusted for further refinement
+    return Math.sqrt(area/Math.PI) * 2;
 }
 
+// Update this function to dynamically use the selected attribute
+function pointToLayer(feature, latlng, attributes){
+    var attribute = attributes[0]; // Default to the first attribute
+    var attValue = feature.properties.PopulationData[attribute];
 
-
-// Function to convert markers to circle markers and add popups
-function pointToLayer(feature, latlng) {
-    // Access the nested PopulationData for the year 2021
-    var attribute = "PopulationData_2021";
-
-    // Create marker options
     var options = {
         fillColor: "#ff7800",
         color: "#000",
         weight: 1,
         opacity: 1,
-        fillOpacity: 0.8
+        fillOpacity: 0.8,
+        radius: calcPropRadius(attValue)
     };
 
-    // For each feature, determine its value for the selected attribute
-    var attValue = feature.properties.PopulationData['2021']; // Access the nested value
-    if (isNaN(attValue)) {
-        attValue = 1; // Default value if attValue is NaN
-    }
-
-    options.radius = calcPropRadius(attValue); // Call the calcPropRadius function
-
-    // Create circle marker layer
     var layer = L.circleMarker(latlng, options);
 
-    // Build popup content string
     var popupContent = "<p><b>Entity Name:</b> " + feature.properties['Entity Name'] + "</p>";
-
-    // Add formatted attribute to the popup content string
-    popupContent += "<p><b>Population in 2021:</b> " + attValue + "</p>";
-
-    // Bind the popup to the circle marker and add an offset
+    popupContent += "<p><b>Population in " + attribute + ":</b> " + attValue + "</p>";
     layer.bindPopup(popupContent, {
         offset: new L.Point(0, -options.radius)
     });
 
-    // Return the circle marker to the L.geoJson pointToLayer option
     return layer;
 }
 
-// Add circle markers for point features to the map
-function createPropSymbols(data, map){
-    // Create a Leaflet GeoJSON layer and add it to the map
+function createPropSymbols(data, map, attributes){
     L.geoJson(data, {
-        pointToLayer: pointToLayer
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes);
+        }
     }).addTo(map);
 }
 
-// Function to retrieve the GeoJSON data and add it to the map with styling and popups
+function processData(data){
+    var attributes = [];
+    var exampleProperties = data.features[0].properties.PopulationData;
+    for (var attribute in exampleProperties) {
+        attributes.push(attribute);
+    }
+    return attributes.sort(); // Make sure to sort the years if necessary
+}
+
+function createSequenceControls(map, attributes){
+    // Use the existing slider
+    var slider = document.querySelector('.range-slider');
+    slider.min = 0;
+    slider.max = attributes.length - 1;
+    slider.value = 0;
+    slider.step = 1;
+
+    // Update the event listener to use the existing slider
+    slider.addEventListener('input', function(){
+        var index = this.value;
+        updatePropSymbols(map, attributes[index]);
+    });
+
+    // Attach event listeners to the existing forward and reverse buttons
+    document.querySelector('#forward').addEventListener('click', function(){
+        if(slider.value < slider.max) {
+            slider.value++;
+        } else {
+            slider.value = 0; // Wrap around to the beginning
+        }
+        updatePropSymbols(map, attributes[slider.value]);
+    });
+
+    document.querySelector('#reverse').addEventListener('click', function(){
+        if(slider.value > 0) {
+            slider.value--;
+        } else {
+            slider.value = slider.max; // Wrap around to the end
+        }
+        updatePropSymbols(map, attributes[slider.value]);
+    });
+}
+
+
+// Function to update the symbols and popups based on the selected attribute
+function updatePropSymbols(map, attribute){
+    map.eachLayer(function(layer){
+        if (layer.feature && layer.feature.properties.PopulationData[attribute]){
+            var props = layer.feature.properties;
+            var radius = calcPropRadius(props.PopulationData[attribute]);
+            layer.setRadius(radius);
+
+            var popupContent = "<p><b>Entity Name:</b> " + props['Entity Name'] + "</p>";
+            popupContent += "<p><b>Population in " + attribute + ":</b> " + props.PopulationData[attribute] + "</p>";
+            layer.getPopup().setContent(popupContent);
+        }
+    });
+}
+
 function getData(map){
-    // Load the data with fetch, then add a GeoJSON layer to the map
     fetch("data/SacramentoRegionPop.geojson")
         .then(function(response){
             return response.json();
         })
         .then(function(json){
-            // Call function to create proportional symbols
-            createPropSymbols(json, map);
+            var attributes = processData(json);
+            createPropSymbols(json, map, attributes);
+            createSequenceControls(map, attributes);
         });
 }
 
-// Event listener that runs the createMap function once the DOM is loaded
 document.addEventListener('DOMContentLoaded', createMap);
+
